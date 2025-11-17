6dddabb3914b4c04cbb7b0cc5befecc0918a1230
Rachana <vukkarachana@gmail.com>
2025-11-17 11:05:06 +0530

    Customer.java

--- src/main/java/com/github/derickfelix/bankapplication/models/Customer.java
+++ src/main/java/com/github/derickfelix/bankapplication/models/Customer.java
@@ -1,34 +1,22 @@
-/*
- * The MIT License
- *
- * Copyright 2019 Derick Felix.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
 package com.github.derickfelix.bankapplication.models;
 
 public class Customer extends Entity {
 
+    private String name;
     private String address;
+    private char sex;
     private String accountNumber;
     private String accountType;
-    private String password;
+    
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
 
     public String getAddress()
     {
@@ -38,6 +26,16 @@ public class Customer extends Entity {
     public void setAddress(String address)
     {
         this.address = address;
+    }
+    
+    public char getSex()
+    {
+        return sex;
+    }
+
+    public void setSex(char sex)
+    {
+        this.sex = sex;
     }
     
     public String getAccountNumber()
@@ -58,16 +56,6 @@ public class Customer extends Entity {
     public void setAccountType(String accountType)
     {
         this.accountType = accountType;
-    }
-
-    public String getPassword()
-    {
-        return password;
-    }
-
-    public void setPassword(String password)
-    {
-        this.password = password;
     }
 
 }
