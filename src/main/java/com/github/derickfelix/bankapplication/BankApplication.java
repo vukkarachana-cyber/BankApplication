40f7dce172e3379167c9791db5f35e7da56c5e8e
Rachana <vukkarachana@gmail.com>
2025-11-17 11:13:54 +0530

    BankApplication.java

--- src/main/java/com/github/derickfelix/bankapplication/BankApplication.java
+++ src/main/java/com/github/derickfelix/bankapplication/BankApplication.java
@@ -1,26 +1,4 @@
-/*
- * The MIT License
- *
- * Copyright 2019 Derick Felix.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
+
 package com.github.derickfelix.bankapplication;
 
 import com.github.derickfelix.bankapplication.utilities.DBUtility;
@@ -30,24 +8,21 @@ import javax.swing.UnsupportedLookAndFeelException;
 
 public class BankApplication {
     
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String args[])
     {
-        /* Set the System look and feel */
-        //<editor-fold defaultstate="collapsed" desc="Look and feel setting code">
+
         try {
             String systemLookAndFeel = javax.swing.UIManager.getSystemLookAndFeelClassName();
             javax.swing.UIManager.setLookAndFeel(systemLookAndFeel);
         } catch (ClassNotFoundException | IllegalAccessException | InstantiationException | UnsupportedLookAndFeelException e) {
             MessageUtility.error(null, "Failed to use system look and feel", e);
         }
-        //</editor-fold>
+      
 
         DBUtility.prepare();
 
-        /* Create and display the form */
+     
         java.awt.EventQueue.invokeLater(() -> {
             new LoginForm().setVisible(true);
         });
